358d50029c74a54e4c86d219050503f7b8d16a5b
Somia Kumari <unibicsomia20@gmail.com>
2025-03-12 11:23:22 +0530

    changes done

--- RAG_PARTS/Retriever.py
+++ RAG_PARTS/Retriever.py
@@ -46,7 +46,7 @@ async def rag(query: str, contexts: list, session_id: str) -> str:
 - Avoid phrases like "I understand". Just reply to the user concisely.
 - Do not go beyond document.
 - Do not ask repeated questions from user,Just guide them towards the next step.
-- if needed respond the user with all the steps involved without skipping any steps.
+- if needed respond the user with all the steps involved without skipping any steps
  -If user ask anything regarding creation then ask question for the reuired field to help them create account and if user asks anything for information then provide the neccessary information from the document.
  
 
