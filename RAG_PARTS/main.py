75f50c4b95e47357bec19df844c952411127dc4b
Somia Kumari <unibicsomia20@gmail.com>
2025-03-12 11:18:54 +0530

    changed port

--- RAG_PARTS/main.py
+++ RAG_PARTS/main.py
@@ -126,4 +126,4 @@ async def send_sms():
 
 if __name__ == "__main__":
     print("in main fn")
-    app.run(debug=True, host='0.0.0.0', port=9000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
