4ef28b1dd3eafc772cd73cf9762968beb70eb968
Somia Kumari <unibicsomia20@gmail.com>
2025-03-25 10:23:52 +0530

    changes done

--- RAG_PARTS/Retriever.py
+++ RAG_PARTS/Retriever.py
@@ -44,7 +44,7 @@ async def rag(query: str, contexts: list, session_id: str) -> str:
 - Ask one relevant question at a time.
 - For action-related queries (e.g., account creation), ask step-by-step required details.
 - Avoid phrases like "I understand". Just reply to the user concisely.
-- Do not go beyond document
+- Do not go beyond document.
 - Do not ask repeated questions from user,Just guide them towards the next step.
 - If needed respond the user with all the steps involved without skipping any steps.
  -If user ask anything regarding creation then ask question for the reuired field to help them create account and if user asks anything for information then provide the neccessary information from the document.
